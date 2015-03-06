dbc119df4a7762c330d37707787e37b8e07a06dd
RyanJamesMcLeod <psystorm21@hotmail.com>
2015-03-05 19:17:58 -0500

    Added Javadoc and tidied code

--- src/java/Servelets/ProductRestStream.java
+++ src/java/Servelets/ProductRestStream.java
@@ -6,15 +6,12 @@
 package Servelets;
 
 import credentials.Credentials;
-import java.io.IOException;
 import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
@@ -37,6 +34,12 @@ import javax.ws.rs.Produces;
 @Path("/stream")
 public class ProductRestStream {
 
+    /**
+     * Performs the GET method that returns all elements of the product database
+     * in JSON array format
+     *
+     * @return returnString
+     */
     @GET
     @Produces("application/json")
     public String doGet() {
@@ -67,6 +70,13 @@ public class ProductRestStream {
         return returnString;
     }
 
+    /**
+     * Performs the GET method that returns the element of the product database
+     * using the user's id entered
+     *
+     * @param id
+     * @return returnString
+     */
     @GET
     @Path("{id}")
     @Produces("application/json")
@@ -75,7 +85,9 @@ public class ProductRestStream {
 
         try (Connection conn = Credentials.getConnection()) {
             JsonObjectBuilder json = Json.createObjectBuilder();
+            
             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM product WHERE ProductId = " + String.valueOf(id));
+            
             ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
@@ -83,10 +95,11 @@ public class ProductRestStream {
                         .add("Name", rs.getString("Name"))
                         .add("Description", rs.getString("Description"))
                         .add("Quantity", rs.getInt("Quantity"));
-
             }
+            
             JsonObject completeJson = json.build();
             returnString = completeJson.toString();
+            
         } catch (SQLException ex) {
             returnString = "SQL Error: " + ex.getMessage();
         }
@@ -94,29 +107,49 @@ public class ProductRestStream {
         return returnString;
     }
 
+    /**
+     * Performs the POST method that will insert a new element into the database
+     * using user-created JSON data
+     *
+     * @param data
+     * @return returnString
+     */
     @POST
     @Consumes("application/json")
-    public String doPost(String data) throws SQLException {
+    public String doPost(String data) {
         JsonReader reader = Json.createReader(new StringReader(data));
         JsonObject json = reader.readObject();
         String returnString = "";
+        
         try (Connection conn = Credentials.getConnection()) {
             String parameters[] = {json.getString("Name"), json.getString("Description"), String.valueOf(json.getInt("Quantity"))};
             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO product (Name, Description, Quantity) VALUES (?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
+            
             for (int i = 1; i <= parameters.length; i++) {
                 pstmt.setString(i, parameters[i - 1]);
             }
+            
             pstmt.executeUpdate();
             ResultSet keys = pstmt.getGeneratedKeys();
+            
             if (keys.next()) {
                 returnString = ("<a>http://localhost:8080/JavaWebAssignment/stream/" + keys.getInt(1) + "</a>");
             }
         } catch (SQLException ex) {
             returnString = "SQL Error: " + ex.getMessage();
         }
+        
         return returnString;
     }
 
+    /**
+     * Performs the POST method that will update an element of the database with
+     * the user JSON data to the id of the user's choice
+     *
+     * @param id
+     * @param data
+     * @return returnString
+     */
     @PUT
     @Path("{id}")
     @Consumes("application/json")
@@ -124,12 +157,15 @@ public class ProductRestStream {
         JsonReader reader = Json.createReader(new StringReader(data));
         JsonObject json = reader.readObject();
         String returnString = "";
+        
         try (Connection conn = Credentials.getConnection()) {
             String parameters[] = {json.getString("Name"), json.getString("Description"), String.valueOf(json.getInt("Quantity"))};
             PreparedStatement pstmt = conn.prepareStatement("UPDATE product SET name = ?, description = ?, quantity = ? WHERE ProductId = ?;", Statement.RETURN_GENERATED_KEYS);
+            
             for (int i = 1; i <= parameters.length; i++) {
                 pstmt.setString(i, parameters[i - 1]);
             }
+            
             pstmt.setInt(4, id);
             pstmt.executeUpdate();
 
@@ -141,12 +177,19 @@ public class ProductRestStream {
         return returnString;
     }
 
+    /**
+     * Performs the DELETE method that will delete the element of the database
+     * using the user's given id
+     *
+     * @param id
+     * @return
+     */
     @DELETE
     @Path("{id}")
     @Produces("application/json")
     public String doDelete(@PathParam("id") int id) {
         String returnString = "";
-        
+
         try (Connection conn = credentials.Credentials.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement("DELETE FROM product WHERE ProductId = ?");
             pstmt.setString(1, String.valueOf(id));
